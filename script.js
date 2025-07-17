5c2e5a56afda57b0a6762d7bcaf5ad80d1cea2c0
alejandroardaya <o.a.ardaya.cau@gmail.com>
2025-07-17 16:59:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,23 +20,47 @@ boton.addEventListener('click', () => {
 
 function playMelody() {
   const notes = [
-    { freq: 261.63, dur: 1.125 },  // DO4
-    { freq: 246.94, dur: 0.375 },  // SI3
-    { freq: 220.00, dur: 0.75 },   // LA3
-    { freq: 196.00, dur: 0.75 },   // SOL3
-    { freq: 329.63, dur: 0.75 },   // MI4
-    { freq: 261.63, dur: 1.125 },  // DO4
-    { freq: 246.94, dur: 1.5 },    // SI3
+    { freq: 261.63, dur: 0.5625 },  // DO4
+    { freq: 246.94, dur: 0.1875 },  // SI3
+    { freq: 220.00, dur: 0.375 },   // LA3
+    { freq: 196.00, dur: 0.375 },   // SOL3
+    { freq: 329.63, dur: 0.375 },   // MI4
+    { freq: 261.63, dur: 0.5625 },  // DO4
+    { freq: 246.94, dur: 0.75 },    // SI3
 
-    { freq: 220.00, dur: 0.75 },   // LA3
-    { freq: 196.00, dur: 0.75 },   // SOL3
-    { freq: 329.63, dur: 0.75 },   // MI4
-    { freq: 261.63, dur: 0.75 },   // DO4
-    { freq: 246.94, dur: 0.75 },   // SI3
-    { freq: 220.00, dur: 1.125 },  // LA3
+    { freq: 220.00, dur: 0.375 },   // LA3
+    { freq: 196.00, dur: 0.375 },   // SOL3
+    { freq: 329.63, dur: 0.375 },   // MI4
+    { freq: 261.63, dur: 0.375 },   // DO4
+    { freq: 246.94, dur: 0.375 },   // SI3
+    { freq: 220.00, dur: 0.5625 },  // LA3
 
-    { freq: 196.00, dur: 0.75 },   // SOL3
-    { freq: 329.63, dur: 0.75 },   // MI4
-    { freq: 196.00, dur: 0.75 },   // SOL3
-    { freq: 261.63, dur: 0.75 },   // DO4
-    { freq: 2
+    { freq: 196.00, dur: 0.375 },   // SOL3
+    { freq: 329.63, dur: 0.375 },   // MI4
+    { freq: 196.00, dur: 0.375 },   // SOL3
+    { freq: 261.63, dur: 0.375 },   // DO4
+    { freq: 246.94, dur: 0.375 },   // SI3
+    { freq: 220.00, dur: 0.5625 }   // LA3
+  ];
+
+  let time = context.currentTime;
+  notes.forEach(note => {
+    const osc = context.createOscillator();
+    const gain = context.createGain();
+
+    osc.type = 'sawtooth'; // trompeta
+    osc.frequency.setValueAtTime(note.freq, time);
+
+    osc.connect(gain);
+    gain.connect(context.destination);
+
+    gain.gain.setValueAtTime(0, time);
+    gain.gain.linearRampToValueAtTime(0.2, time + 0.02); // entrada más rápida
+    gain.gain.linearRampToValueAtTime(0, time + note.dur); // fade out
+
+    osc.start(time);
+    osc.stop(time + note.dur);
+
+    time += note.dur; // sin pausa
+  });
+}
