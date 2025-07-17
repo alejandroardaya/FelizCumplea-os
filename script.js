9ed27b2c823805b71519e15378e165efc98cbb47
alejandroardaya <o.a.ardaya.cau@gmail.com>
2025-07-17 16:47:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,20 +20,19 @@ boton.addEventListener('click', () => {
 
 function playMelody() {
   const notes = [
-    // Intro de La Vie en Rose (Louis Armstrong)
-    { freq: 440, dur: 0.7 },  // A4
-    { freq: 493.88, dur: 0.7 }, // B4
-    { freq: 523.25, dur: 1.2 }, // C5
-    { freq: 440, dur: 0.6 }, 
-    { freq: 523.25, dur: 0.6 }, 
-    { freq: 587.33, dur: 1.1 }, // D5
-    { freq: 659.25, dur: 0.7 }, // E5
-    { freq: 587.33, dur: 0.7 }, 
-    { freq: 523.25, dur: 1.2 },
-    { freq: 493.88, dur: 0.8 },
-    { freq: 440, dur: 1.2 },
-    { freq: 392.00, dur: 1.2 }, // G4
-    { freq: 440.00, dur: 1.5 }
+    { freq: 440, dur: 1.4 },      // A4
+    { freq: 493.88, dur: 1.4 },   // B4
+    { freq: 523.25, dur: 2.4 },   // C5
+    { freq: 440, dur: 1.2 }, 
+    { freq: 523.25, dur: 1.2 }, 
+    { freq: 587.33, dur: 2.2 },   // D5
+    { freq: 659.25, dur: 1.4 },   // E5
+    { freq: 587.33, dur: 1.4 }, 
+    { freq: 523.25, dur: 2.4 },
+    { freq: 493.88, dur: 1.6 },
+    { freq: 440, dur: 2.4 },
+    { freq: 392.00, dur: 2.4 },   // G4
+    { freq: 440.00, dur: 3.0 }
   ];
 
   let time = context.currentTime;
@@ -45,15 +44,15 @@ function playMelody() {
     gain.connect(context.destination);
 
     osc.frequency.setValueAtTime(note.freq, time);
-    osc.type = 'sawtooth'; // simula timbre de trompeta
+    osc.type = 'sawtooth'; // simula trompeta
 
     gain.gain.setValueAtTime(0, time);
-    gain.gain.linearRampToValueAtTime(0.2, time + 0.05); // fade in
+    gain.gain.linearRampToValueAtTime(0.15, time + 0.1); // fade in
     gain.gain.linearRampToValueAtTime(0, time + note.dur); // fade out
 
     osc.start(time);
-    osc.stop(time + note.dur + 0.1);
+    osc.stop(time + note.dur + 0.2);
 
-    time += note.dur + 0.1;
+    time += note.dur + 0.2;
   });
 }
