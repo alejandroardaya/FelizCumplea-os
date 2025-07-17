90109302923b91682122950b9a60c4e7887eb2b7
alejandroardaya <o.a.ardaya.cau@gmail.com>
2025-07-17 16:57:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -39,28 +39,4 @@ function playMelody() {
     { freq: 329.63, dur: 0.75 },   // MI4
     { freq: 196.00, dur: 0.75 },   // SOL3
     { freq: 261.63, dur: 0.75 },   // DO4
-    { freq: 246.94, dur: 0.75 },   // SI3
-    { freq: 220.00, dur: 1.125 }   // LA3
-  ];
-
-  let time = context.currentTime;
-  notes.forEach(note => {
-    const osc = context.createOscillator();
-    const gain = context.createGain();
-
-    osc.type = 'sawtooth'; // timbre similar a trompeta
-    osc.frequency.setValueAtTime(note.freq, time);
-
-    osc.connect(gain);
-    gain.connect(context.destination);
-
-    gain.gain.setValueAtTime(0, time);
-    gain.gain.linearRampToValueAtTime(0.2, time + 0.05); // fade in
-    gain.gain.linearRampToValueAtTime(0, time + note.dur); // fade out
-
-    osc.start(time);
-    osc.stop(time + note.dur + 0.1);
-
-    time += note.dur + 0.1; // leve pausa entre notas
-  });
-}
+    { freq: 2
